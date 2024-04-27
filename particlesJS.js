3e3dab2a1a5976c87a186934c5dbfaba9edab46a
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-04-27 18:19:48 -0400

    Update particlesJS.js

--- particlesJS.js
+++ particlesJS.js
@@ -1,1 +1,4 @@
-particlesJS.load('particles-js', './particles.json');
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
