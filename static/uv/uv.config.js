ed26a74db8ecc4edb54e43fc046f825b792f8805
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-05-21 16:25:41 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/tiw',
+    prefix: '/static/tiw/',
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
