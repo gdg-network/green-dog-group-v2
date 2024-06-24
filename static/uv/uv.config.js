0f8147ce1642c5652b2780e78734924078a640b0
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-06-24 14:01:16 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://uv-bare-vercel.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
