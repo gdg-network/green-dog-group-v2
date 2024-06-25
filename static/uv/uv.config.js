63c520d93b0d0c25eb36b65beedfd29178bb829e
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-06-25 10:25:11 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/greendog/gdg',
     bare:'https://uv-bare-vercel.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
